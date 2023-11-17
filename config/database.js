245077875aeed91d6d7d4d1132f83359e4f27ec7
socratic617 <erik.solis02135@gmail.com>
2023-11-17 02:04:15 -0500

    set up mongo db

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://mongDB:happy123!@cluster0.c1bg23b.mongodb.net/taskmanagement?retryWrites=true&w=majority', 
+    'dbName': 'taskmanagement'
+};
