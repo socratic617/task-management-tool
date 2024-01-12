031ed1ff3959afbb99432e007ab27bd5546fc11c
socratic617 <erik.solis02135@gmail.com>
2024-01-12 00:18:47 -0500

    troubleshooting bug for env

--- config/database.js
+++ config/database.js
@@ -1,26 +1,26 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.DB_STRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true,
-        });
+// const connectDB = async () => {
+//     try {
+//         const conn = await mongoose.connect(process.env.DB_STRING, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//             useFindAndModify: false,
+//             useCreateIndex: true,
+//         });
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
-};
+//         console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     } catch (err) {
+//         console.error(err);
+//         process.exit(1);
+//     }
+// };
 
-module.exports = connectDB;
+// module.exports = connectDB;
 
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://mongDB:happy123!@cluster0.c1bg23b.mongodb.net/taskmanagement?retryWrites=true&w=majority', 
+    'url': process.env.DB_STRING, 
     'dbName': 'taskmanagement'
 };
